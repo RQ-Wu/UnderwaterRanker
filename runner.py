b8fcf08767933bf885b5c51800f415e7eff879b7
RQ-Wu <1050562365@qq.com>
2022-04-23 23:46:35 -0400

    resume training

--- runner.py
+++ runner.py
@@ -196,7 +196,11 @@ class Ranker_Runner():
         srocc_list = []
         krocc_list = []
 
-        for epoch in range(self.training_opt['epoch']):
+        if self.model_opt['resume_ckpt_path']:
+            ckpt = torch.load(self.model_opt['resume_ckpt_path'])
+            self.optimizer.load_state_dict(ckpt['optimizer'])
+            start_epoch = ckpt['epoch']
+        for epoch in range(start_epoch, self.training_opt['epoch']):
             print('================================ %s %d / %d ================================' % (self.experiments_opt['save_root'].split('/')[-1], epoch, self.training_opt['epoch']))
             loss = self.train_loop(epoch)
             torch.cuda.empty_cache()
